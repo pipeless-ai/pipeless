97a7f60e75b3b7854e0d81280e9c64d9ff504148
Miguel Angel Cabrera Minagorri <devgorri@gmail.com>
2023-07-21 19:21:05 +0200

    [cli] Return frames in example app
    Signed-off-by: Miguel Angel Cabrera Minagorri <devgorri@gmail.com>

--- cli/cli/commands/templates/default/apps/example.py
+++ cli/cli/commands/templates/default/apps/example.py
@@ -4,6 +4,9 @@ class Example(app.App):
     """
     Main application class.
 
+    Pre-process, process and post-process hooks, if implemented,
+    must return the frame if implemented.
+
     Note the main class MUST be called like the app
     file to be loaded properly.
 
@@ -17,15 +20,18 @@ class Example(app.App):
 
     # Hook to execute to pre-process each frame
     def pre_process(self, frame, ctx):
-        pass
+        modified_frame = frame # Do something to the frame
+        return modified_frame
 
     # Hook to execute to process each frame
     def process(self, frame, ctx):
-        pass
+        modified_frame = frame # Do something to the frame
+        return modified_frame
 
     # Hook to execute after processing each frame
     def post_process(self, frame, ctx):
-        pass
+        modified_frame = frame # Do something to the frame
+        return modified_frame
 
     # Hook to execute after the processing loop
     def after(self, ctx):
