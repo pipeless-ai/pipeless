4f1e0464bb1963de8c0378c1a2d12e19392bb8db
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-08-03 20:09:08 +0200

    Add direct input to output communication sockets
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- cloud/pupila/lib/connection.py
+++ cloud/pupila/lib/connection.py
@@ -1,11 +1,38 @@
-from pynng import Push0, Pull0, Timeout
+from pynng import Push0, Pull0, Timeout, Pair0
 
 from .singleton import Singleton
 from .config import Config
 from .logger import logger
 
-# TODO: create a InputBusSocket/OutputBusSocket to send metadata messages
-#       to all connected workers in order to update the pipelines when the stream type changes
+class InputOutputSocket(metaclass=Singleton):
+    """
+    nng socket to send messages from the input to the output
+    """   
+    def __init__(self, mode):
+        """
+        Parameters:
+        - mode: 'w' for the input (write). 'r' for the output (read)
+        """
+        config = Config(None) # Get the already existing config instance
+        address = config.get_input().get_address()
+        # Make this connection to run on the provided port+1. 
+        # The provided port is for other type of connection
+        self._addr = f'tcp://{address.get_host():{address.get_port() + 1}}'
+        if mode == 'w':
+            self._socket = Pair0(listen=self._addr)
+        elif mode == 'r':
+            self._socket = Pair0(listen=self._addr)
+        else:
+            raise 'Wrong mode for InputOutputSocket'
+
+    def send(self, msg):
+        self.socket.send(msg)
+    def recv(self, timeout):
+        try:
+            return self.socket.recv(timeout=timeout)
+        except Timeout:
+            logger.warning("Timeout receiving metadata from input")
+            return None
 
 class InputPushSocket(metaclass=Singleton):
     """
