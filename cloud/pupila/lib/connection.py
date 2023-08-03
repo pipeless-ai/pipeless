6221f79bcfbae8eb6e38eeda3fd57607f52727e7
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-08-03 17:03:21 +0200

    Add output sockets
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- cloud/pupila/lib/connection.py
+++ cloud/pupila/lib/connection.py
@@ -4,9 +4,12 @@ from .singleton import Singleton
 from .config import Config
 from .logger import logger
 
-class PushSocket(metaclass=Singleton):
+# TODO: create a InputBusSocket/OutputBusSocket to send metadata messages
+#       to all connected workers in order to update the pipelines when the stream type changes
+
+class InputPushSocket(metaclass=Singleton):
     """
-    nng push socket to share messages
+    nng push socket to push messages from the input to the workers
     """
     def __init__(self):
         config = Config(None) # Get the already existing config instance
@@ -17,9 +20,22 @@ class PushSocket(metaclass=Singleton):
     def send(self, msg):
         self.socket.send(msg)
 
-class PullSocket(metaclass=Singleton):
+class OutputPushSocket(metaclass=Singleton):
     """
-    nng pull socket to share messages
+    nng push socket to push messages from the workers to the output
+    """
+    def __init__(self):
+        config = Config(None) # Get the already existing config instance
+        address = config.get_output().get_address()
+        self._addr = f'tcp://{address.get_address()}'
+        self._socket = Push0(listen=self._addr)
+
+    def send(self, msg):
+        self.socket.send(msg)
+
+class InputPullSocket(metaclass=Singleton):
+    """
+    nng pull socket to fetch messages from the input to the workers
     """
     def __init__(self):
         config = Config(None) # Get the already existing config instance
@@ -27,8 +43,26 @@ class PullSocket(metaclass=Singleton):
         self._addr = f'tcp://{address.get_address()}'
         self._socket = Pull0(listen=self._addr)
 
-    def recv(self):
+    def recv(self, timeout):
         try:
-            return self.socket.recv()
+            return self.socket.recv(timeout=timeout)
         except Timeout:
+            logger.warning("Timeout waiting for new frames")
+            return None
+
+class OutputPullSocket(metaclass=Singleton):
+    """
+    nng pull socket to fetch messages from the workers to the output
+    """
+    def __init__(self):
+        config = Config(None) # Get the already existing config instance
+        address = config.get_output().get_address()
+        self._addr = f'tcp://{address.get_address()}'
+        self._socket = Pull0(listen=self._addr)
+
+    def recv(self, timeout):
+        try:
+            return self.socket.recv(timeout=timeout)
+        except Timeout:
+            logger.warning("Timeout waiting for new frames")
             return None
