5f3a39b98fd3ce07d9dcac28ddf00d14d7dc8888
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-08-03 20:12:42 +0200

    (input) Send messages to output on new streams
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- cloud/pupila/lib/input/input.py
+++ cloud/pupila/lib/input/input.py
@@ -1,5 +1,5 @@
+import sys
 import traceback
-import numpy as np
 
 import gi
 gi.require_version('GLib', '2.0')
@@ -8,10 +8,10 @@ gi.require_version('Gst', '1.0')
 gi.require_version('GstApp', '1.0')
 from gi.repository import Gst, GObject, GstApp
 
-from ..logger import logger
-from ..connection import InputPushSocket
-from ..config import Config
-from cloud.pupila.lib.messages import RgbImageMsg
+from cloud.pupila.lib.logger import logger
+from cloud.pupila.lib.connection import InputOutputSocket, InputPushSocket
+from cloud.pupila.lib.config import Config
+from cloud.pupila.lib.messages import RgbImageMsg, StreamMetadataMsg
 
 def on_new_sample(sink: GstApp.AppSink) -> Gst.FlowReturn:
     sample = sink.emit("pull-sample")
@@ -40,7 +40,7 @@ def on_new_sample(sink: GstApp.AppSink) -> Gst.FlowReturn:
 
     # Pass msg to the workers
     s_push = InputPushSocket()
-    s_push.send(msg)
+    s_push.send(msg.serialize())
 
     # Release resources
     buffer.unmap(info)
@@ -54,9 +54,11 @@ def on_bus_message(bus: Gst.Bus, msg: Gst.Message, loop: GObject.MainLoop):
     the processing, we can catch it and stop the pipeline here.
     """
     mtype = msg.type
-    if mtype == Gst.MessageType.EOS:
+    if mtype == Gst.MessageType.NEW_STREAM:
+        stream_id = msg.get_structure().get_string("stream-id")
+        logger.info(f"New stream detected with ID: {stream_id}")
+    elif mtype == Gst.MessageType.EOS:
         logger.info("End-Of-Stream reached.")
-        loop.quit()
     elif mtype == Gst.MessageType.ERROR:
         err, debug = msg.parse_error()
         logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
@@ -68,6 +70,28 @@ def on_bus_message(bus: Gst.Bus, msg: Gst.Message, loop: GObject.MainLoop):
         logger.warning(f"Debugging information: {debug if debug else 'none'}")
 
     return True
+
+def notify_stream_to_output(elem, pad):
+    caps = pad.get_current_caps()
+    """
+     TODO: if the pad name is always different, we can run simultaneous
+     pipelines in both input and output by simply creating a create_pipeline
+     function and managing several of them in both the input and the output.
+     That would allow users to send several streams to the same app.
+     they would be processed on the same workers with the same code.
+     How do we distinguish the videos for the output? Right now
+     they will both be sent to the same URI
+    """
+    logger.info(f'input pad name: {pad.get_name()}')
+    logger.info(f'New pad added to uridecodebin with caps {caps}')
+    if caps:
+        m_socket = InputOutputSocket('w')
+        m_msg = StreamMetadataMsg(caps)
+        m_socket.send(m_msg.serialize())
+
+def on_pad_added(element, pad, *callbacks):
+    for callback in callbacks:
+        callback(pad)
 
 def input():
     # Load config
@@ -86,16 +110,16 @@ def input():
 
     if not pipeline:
         logger.error('Failed to create pipeline')
-        return
+        sys.exit(1)
     if not uridecodebin:
         logger.error('Failed to create uridecodebin')
-        return
+        sys.exit(1)
     if not videoconvert:
         logger.error('Failed to create videoconvert')
-        return
+        sys.exit(1)
     if not appsink:
         logger.error("Failed to create appsink.")
-        return
+        sys.exit(1)
 
     # Set properties for elements
     appsink.set_property("emit-signals", True)
@@ -110,7 +134,7 @@ def input():
     # Link static elements (fixed number of pads): uridecoder (linked later) -> videoconvert -> appsink
     if not videoconvert.link(appsink):
         logger.error("Failed to link appsink to videoconvert")
-        return
+        sys.exit(1)
 
     videoconvert_sink_pad = videoconvert.get_static_pad("sink")
     # Link dynamic elements (dynamic number of pads)
@@ -123,11 +147,18 @@ def input():
         else:
             logger.warning('Video converter pad is already linked. Skipping uridecoder link')
 
-    uridecodebin.connect("pad-added", pad_added_callback)
+    uridecodebin.connect(
+        "pad-added",
+        lambda element, pad:
+          on_pad_added(
+            element, pad, pad_added_callback, notify_stream_to_output
+        )
+    )
+
+    loop = GObject.MainLoop()
 
     # Handle bus events on the main loop
     bus = pipeline.get_bus()
-    loop = GObject.MainLoop()
     bus.add_signal_watch()
     bus.connect("message", on_bus_message, loop)
 
@@ -135,7 +166,7 @@ def input():
     ret = pipeline.set_state(Gst.State.PLAYING)
     if ret == Gst.StateChangeReturn.FAILURE:
         logger.error("Unable to set the pipeline to the playing state.")
-        return
+        sys.exit(1)
 
     try:
         logger.debug(f'uridecodebin state: {uridecodebin.get_state(5)}')
