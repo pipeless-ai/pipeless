93b1fdb359f64634920cf31297c501d6c8d8ee23
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-08-25 13:14:28 +0200

    feat: install user packages on worker
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

---
+++ package/scripts/libpipeless.sh
@@ -0,0 +1,49 @@
+
+########################
+# Install Python dependencies required by the user code to run the worker
+# Globals:
+#   PIPELESS_*
+# Arguments:
+#   None
+# Returns:
+#   None
+#########################
+pipeless_install_user_python_deps() {
+    [[ -z "$PIPELESS_USER_PYTHON_PACKAGES" ]] && return
+
+    local -a package_list
+    read -r -a package_list <<< "$(tr ',;' ' ' <<< "${PIPELESS_USER_PYTHON_PACKAGES}")"
+    if [[ "${#package_list[@]}" -le 0 ]]; then
+        echo "No python packages specified by the user"
+        return
+    fi
+
+    for package in "${package_list[@]}"; do
+	pip install "$package"
+    done
+}
+
+########################
+# Install system packages required by the user code to run the worker
+# These need to be installed at buildtime
+# Globals:
+#   PIPELESS_*
+# Arguments:
+#   None
+# Returns:
+#   None
+#########################
+pipeless_install_user_system_deps() {
+    [[ -z "$PIPELESS_USER_SYSTEM_PACKAGES" ]] && return
+
+    local -a package_list
+    read -r -a package_list <<< "$(tr ',;' ' ' <<< "${PIPELESS_USER_SYSTEM_PACKAGES}")"
+    if [[ "${#package_list[@]}" -le 0 ]]; then
+        echo "No system packages specified by the user"
+        return
+    fi
+
+    for package in "${package_list[@]}"; do
+	apt-get install "$package"
+    done
+}
