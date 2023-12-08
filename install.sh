f03df6c3fcd1e8a5d305e5b011fb2b0f49fafb11
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2023-12-08 13:41:00 +0100

    fix: add gstreamer rtsp package to install script
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- install.sh
+++ install.sh
@@ -205,7 +205,7 @@ libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base \
 gstreamer1.0-plugins-good gstreamer1.0-plugins-bad \
 gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools \
 gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 \
-gstreamer1.0-qt5 gstreamer1.0-pulseaudio"
+gstreamer1.0-qt5 gstreamer1.0-pulseaudio gstreamer1.0-rtsp"
     elif [ "${OS}" == "darwin" ]; then
       echo "brew install gstreamer"
     fi
