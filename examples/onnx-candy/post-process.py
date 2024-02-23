337dccfeb88e480ef74fb66f716f1b00b225ae9e
Miguel A. Cabrera Minagorri <devgorri@gmail.com>
2024-02-23 13:00:29 +0100

    Add YOLO World example
    Signed-off-by: Miguel A. Cabrera Minagorri <devgorri@gmail.com>

--- examples/onnx-candy/post-process.py
+++ examples/onnx-candy/post-process.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 
 def hook(frame_data, _):
-    inference_results = frame_data["inference_output"]
+    inference_results = frame_data["inference_output"].get("output1", [])
     candy_image = inference_results[0] # Remove batch axis
     candy_image = np.clip(candy_image, 0, 255)
     candy_image = candy_image.transpose(1,2,0).astype("uint8")
